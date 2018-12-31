8eb779ce91c5c128512405298418748bc3d15c85
Dane Hollenbach <danehollenbach@gmail.com>
2018-12-31 14:18:25 +0200

    Update constants.js

--- app/config/constants.js
+++ app/config/constants.js
@@ -2,6 +2,6 @@ export default {
   keyboardAvoidingOffset: 75,
   company_id: "reex",
   rehive_url: "https://api.rehive.com/3/",
-  reex_service_url: "http://reecore-reex-vm.northeurope.cloudapp.azure.com/api/v1/reex/",
+  reex_service_url: "https://your-service-endpoint/api/v1/reex/",
   reex_explorer: "http://explorer.reecore.org/tx/"
 }
